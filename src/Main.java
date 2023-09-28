334adcde722c054e9b4c24f0d2b986d4f1c3b129
Mark Mesander <mark_mesander@hotmail.com>
2023-09-28 19:32:56 +0200

    add 2 water pokemons and 2 grass pokemons to pokemons arraylist

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,10 @@ public class Main {
         pokemons.add(new ElectricPokemon("Zapdos", 1.6, 52.6, "zap zap", "bird food", "pressure", "ice and rock", 80, 160));
         pokemons.add(new FirePokemon("Charizard", 1.7, 90.5, "char char", "dragon food", "blaze", "water, electric and rock", 200, 800));
         pokemons.add(new FirePokemon("Arcanine", 1.9, 155.0, "ar ar", "tiger food", "flash fire", "water, ground and rock", 60, 215));
+        pokemons.add(new WaterPokemon("Blastoise", 1.6, 85.5, "blas blas", "turtle food", "torrent", "grass and electric", 160, 320));
+        pokemons.add(new WaterPokemon("Psyduck", 0.8, 19.6, "psy psy", "duck food", "cloud nine", "grass and electric", 20, 160));
+        pokemons.add(new GrassPokemon("Venusaur", 2.0, 100, "venu venu", "pad food", "overgrow", "fire, psychic, flying and ice", 125, 415));
+        pokemons.add(new GrassPokemon("Exeggutor", 2.0, 120.0, "exegu exegu", "tree food", "chlorophyll", "ghost, fire, ice, dark and poison", 85, 215));
 
 
         for (Pokemon pokemon : pokemons) {
