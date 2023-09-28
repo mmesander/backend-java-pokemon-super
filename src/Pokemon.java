2bdfdbd6c7ba8bbe908df0e11f3963a84fa1c547
Mark Mesander <mark_mesander@hotmail.com>
2023-09-28 15:44:46 +0200

    add super class Pokemon, and constructor

---
+++ src/Pokemon.java
@@ -0,0 +1,20 @@
+package src;
+
+public class Pokemon {
+    private String name;
+    private int weight;
+    private int hp;
+    private String attack;
+    private String food;
+
+    public Pokemon(String name, int weight, int hp, String attack, String food){
+        this.name = name;
+        this.weight = weight;
+        this.hp = hp;
+        this.attack = attack;
+        this.food = food;
+    }
+
+
+
+}
